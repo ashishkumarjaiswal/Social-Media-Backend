fbe98982eefb6438da9ffdcd91eee4ccd353e3b5
Ashish <jayswal99299@gmail.com>
2023-03-04 16:29:56 +0530

    cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv")
+const cors = require("cors");
+
 const app = express();
 
 dotenv.config()
@@ -8,6 +10,15 @@ dotenv.config()
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            callback(null, true);
+        },
+        credentials: true,
+    })
+);
 
 const user = require("./routes/user");
 const post = require("./routes/post");
